f3f2a8b88668bf7239dc9ce3177b8c3e8e4955f7
neskarlymarin1001-del <neskarlymarin1001@gmail.com>
2025-08-27 13:50:33 -0500

    Add files via upload

---
+++ ClassWork-1.py
@@ -0,0 +1,23 @@
+#Problem 1 and 2
+
+"""print("Welcome to OOP class")
+name_name = input("Enter the student name: ")
+
+course1 = int(input("Enter the grade for course1: "))
+course2 = int(input("Enter the grade for course2: "))
+course3 = int(input("Enter the grade for course3: "))
+course4 = int(input("Enter the grade for course4: "))
+course5 = int(input("Enter the grade for course5: "))
+
+total = course1 + course2 + course3 + course4 + course5
+print ("Your total points are:", total)
+
+course_quantity = 5
+average_grade = total/course_quantity
+
+print ("Your average grade is:", average_grade)
+"""
+
+#Problem 3
+
+employee_name =
