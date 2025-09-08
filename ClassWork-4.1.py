61b1fe0f880f0e546421073ee6d0bd198d0cb30a
neskarlymarin1001-del <neskarlymarin1001@gmail.com>
2025-09-08 13:50:25 -0500

    Add files via upload

---
+++ ClassWork-4.1.py
@@ -0,0 +1,41 @@
+print("Welcome!")
+
+myList = []
+while True:
+    print("Please choose one of the following: ")
+    print("1. Add an element to the list")
+    print("2. Remove a specific element from the list")
+    print("3. Pop the last element")
+    print("4. Display the list")
+    print("5. Quit")
+
+    option = input("Please enter your choice here: ")
+
+    if option == "1":
+        newElement = input("Please provide the element you want to add: ")
+        myList.append(newElement)
+
+    elif option == "2":
+        if len(myList) == 0:
+            print("The list is empty")
+        else:
+            print("These are the elements on the list: ")
+            print(myList)
+            oldElement = input("Please provide the element you want to remove: ")
+            if oldElement not in myList:
+                print("That element is not in the list")
+            else:
+                myList.remove(oldElement)
+
+    elif option == "3":
+        if len(myList) == 0:
+            print("The list is empty")
+        else:
+            myList.pop()
+
+    elif option == "4":
+        print(myList)
+
+    elif option == "5":
+        break
+
---
+++ ClassWork-4.py
@@ -0,0 +1,14 @@
+myList = [2,4,8,54,"Justus"]
+
+myList.append(92)
+print(myList)
+
+myList.remove(54)
+print(myList)
+
+myList.pop()
+print(myList)
+
+myList[1] = "Orange"
+myList[2] = "Pink"
+print(myList)
