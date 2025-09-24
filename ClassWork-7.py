2fca00d7a43869a7c80bcc7656ba26f80e19f433
neskarlymarin1001-del <neskarlymarin1001@gmail.com>
2025-09-24 10:09:52 -0500

    Update ClassWork-7.py

--- ClassWork-7.py
+++ ClassWork-7.py
@@ -56,14 +56,68 @@ while True:
                 del Projects[oldProject]
 
     elif option == "3":
-        if len(Projects) == 0:
-            print("The dictionary is empty")
+    if len(Projects) == 0:
+        print("The dictionary is empty")
+    else:
+        oldProject = input("Please provide the ID of the project you want to update: ")
+        if oldProject not in Projects:
+            print("That project is not in the dictionary")
         else:
-            oldProject = input("Please provide the ID of the project you want to update: ")
-            if oldProject not in Projects:
-                print("That project is not in the dictionary")
+            print("What would you like to update?")
+            print("1. Project Title")
+            print("2. Managers")
+            print("3. Start Date")
+            print("4. End Date")
+            print("5. Sponsors")
+            print("6. Budget")
+            print("7. Technologies")
+            print("8. Team Members")
+
+            choice = input("Enter the number of the field you want to update: ")
+
+            if choice == "1":
+                new_value = input("Enter new Project Title: ")
+                Projects[oldProject]["Project_Title"] = new_value
+
+            elif choice == "2":
+                new_list = []
+                n = int(input("How many managers do you want to add? "))
+                for i in range(n):
+                    new_list.append(input("Enter manager name: "))
+                Projects[oldProject]["Managers"] = new_list
+
+            elif choice == "3":
+                new_value = input("Enter new Start Date: ")
+                Projects[oldProject]["Start_Date"] = new_value
+
+            elif choice == "4":
+                new_value = input("Enter new End Date: ")
+                Projects[oldProject]["End_Date"] = new_value
+
+            elif choice == "5":
+                new_value = input("Enter new Sponsor: ")
+                Projects[oldProject]["Sponsors"] = new_value
+
+            elif choice == "6":
+                new_value = float(input("Enter new Budget: "))
+                Projects[oldProject]["Budget"] = new_value
+
+            elif choice == "7":
+                new_list = []
+                n = int(input("How many technologies do you want to add? "))
+                for i in range(n):
+                    new_list.append(input("Enter technology: "))
+                Projects[oldProject]["Technologies"] = new_list
+
+            elif choice == "8":
+                new_list = []
+                n = int(input("How many team members do you want to add? "))
+                for i in range(n):
+                    new_list.append(input("Enter team member: "))
+                Projects[oldProject]["Team_Members"] = new_list
+
             else:
-                value = input("Please provide the value of the dictionary you want to update: ")
+                print("Invalid option")
 
     elif option == "4":
         for project_record in Projects.items():
@@ -74,6 +128,7 @@ while True:
         break
 
 print("Thank you for using this program!")
+
 
 
 
