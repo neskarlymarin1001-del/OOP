3b179eeb4ef01d51726333280952a85b394e5577
neskarlymarin1001-del <neskarlymarin1001@gmail.com>
2025-10-24 13:53:49 -0500

    Add files via upload

--- Lab 4.py
+++ Lab 4.py
@@ -6,6 +6,8 @@ class Customer:
         self.phone = ""
         self.emailID = ""
         self.balance = 0.0
+        self.credit_card = []
+        self.debit_card = ""
 
     def add_values_to_Customer(self):
         self.cid = input("Enter Customer ID: ")
@@ -29,6 +31,43 @@ class Customer:
         print("Phone Number: ", self.phone)
         print("Email: ", self.emailID)
         print("Balance: ", self.balance)
+        print("Debit Card: ")
+        self.debit_card.display_Card_info()
+        if not self.credit_card:
+            print("No Credit Cards")
+        else:
+            print("Credit Cards:")
+            for card in self.credit_card:
+                card.display_Card_info()
+
+    def assign_card(self, card_object):
+        if card_object.type == "debit":
+            self.debit_card = card_object
+        elif card_object.type == "credit":
+            self.credit_card.append(card_object)
+
+
+class Card:
+    def __init__(self):
+        self.type = ""
+        self.card_no = ""
+        self.cvv = ""
+        self.expiry_date = ""
+        self.card_balance = 0.0
+
+    def add_values_to_Card(self):
+        self.type = input("Enter the Card Type (credit or debit): ")
+        self.card_no = input("Enter Card Number: ")
+        self.cvv = input("Enter Card CVV: ")
+        self.expiry_date= input("Enter Card Expiry Date: ")
+
+    def display_Card_info(self):
+        print("Type: ", self.type)
+        print("Card No: ", self.card_no)
+        print("CVV: ", self.cvv)
+        print("Expiry Date: ", self.expiry_date)
+        print("Card Balance: ", self.card_balance)
+
 
 
 # Main Code
@@ -36,12 +75,18 @@ class Customer:
 c1 = Customer ()
 c2 = Customer ()
 
-c1.add_values_to_Customer()
-c2.add_values_to_Customer()
+card1 = Card()
+card2 = Card()
 
-amount = float(input("Please enter amount to be credited and debited: "))
-c1.debit_from(amount)
-c2.credit_to(amount)
+c1.add_values_to_Customer()
+#c2.add_values_to_Customer()
+
+card1.add_values_to_Card()
+#card2.add_values_to_Card()
+
+c1.assign_card(card1)
+#c2.assign_card(card2)
 
 c1.display_Cust_info()
-c2.display_Cust_info()
+#c2.display_Cust_info()
+
